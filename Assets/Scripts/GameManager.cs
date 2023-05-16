6a8e55e566788c4d972dc75c3e10de1b8067431f
Whizzyants <124131184+SrivishnuWhizzyAnts@users.noreply.github.com>
2023-05-16 10:23:37 +0530

    Implemented Internet Check

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,8 +10,50 @@ public class GameManager : MonoBehaviour
     [field: Header("For BallInput Script")]
     [field: SerializeField] public GameObject ringObj { get; private set; } //Used as reference to calculate force for ball by BallInput Script
 
+    [field: Header("Internet Connection Checking")]
+    [field: SerializeField] private float internetConnectionCheckInterval = 3f;
+    [field: SerializeField] private bool shouldCheckForInternetConnection = true;
+    public Action IsConnectedToInternet, IsDisconnectedFromInternet;
+    private bool whenConnectedActionsCarriedOut = false;
+    private bool whenDisconnectedActionsCarriedOut = false;
+
     private void Awake()
     {
         instance = this;
+    }
+    private void Start()
+    {
+        StartCoroutine(CheckInternetConnection());
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private IEnumerator CheckInternetConnection()
+    {
+        while (shouldCheckForInternetConnection)
+        {
+            if (Application.internetReachability == NetworkReachability.NotReachable)
+            {
+                if(!whenDisconnectedActionsCarriedOut)
+                {
+                    IsDisconnectedFromInternet?.Invoke();
+                    whenDisconnectedActionsCarriedOut = true;
+                    whenConnectedActionsCarriedOut = false;
+                }
+            }
+            else
+            {
+                if(!whenConnectedActionsCarriedOut)
+                {
+                    IsConnectedToInternet?.Invoke();
+                    whenConnectedActionsCarriedOut = true;
+                    whenDisconnectedActionsCarriedOut = false;
+                }
+            }
+            yield return new WaitForSeconds(internetConnectionCheckInterval);
+        }
     }
 }
