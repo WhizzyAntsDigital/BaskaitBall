6cf587dee8882d5ade6a2af0fbb607c16093540c
SrivishnuSrinivasan <srivishnu@whizzyants.com>
2023-05-17 12:24:49 +0530

    Tournament

---
+++ Assets/Scripts/Tournament/TournamentBracket.cs
@@ -0,0 +1,21 @@
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TournamentBracket : MonoBehaviour
+{
+    [field: Header("Tournament Bracket Generator")]
+    [field: SerializeField] private TextMeshProUGUI playerUsername;
+    [field: SerializeField] private TextMeshProUGUI opponentUsername;
+    [field: SerializeField] private TextMeshProUGUI roundTitle;
+    [field: SerializeField] private Image playerIcon;
+    [field: SerializeField] private Image opponentIcon;
+
+    private void Start()
+    {
+        string[] roundsNames = { "Semi Finals", "Finals" };
+        playerUsername.text = AINamesGenerator.Utils.GetRandomName();
+        opponentUsername.text = AINamesGenerator.Utils.GetRandomName();
+        roundTitle.text = roundsNames[Random.Range(0, 2)];
+    }
+}
